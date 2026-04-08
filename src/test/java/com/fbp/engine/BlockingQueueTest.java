17bea06f9743048c96064acbb8e804fe8968bd99
JJ <wognlrla1@gmail.com>
2026-04-08 17:51:03 +0900

    s

--- src/test/java/com/fbp/engine/BlockingQueueTest.java
+++ src/test/java/com/fbp/engine/BlockingQueueTest.java
@@ -1,10 +1,27 @@
 package com.fbp.engine;
 
+import com.fbp.engine.core.Connection;
+import com.fbp.engine.message.Message;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BlockingQueueTest {
     @Test
-    void testDeliverPoll(){
+    void testDeliverPoll() throws InterruptedException {
+        Connection connection = new Connection("c1");
+
+        Message message = new Message(Map.of("data","test"));
+
+        connection.deliver(message);
+        Message result = connection.poll();
+        assertEquals(message,result);
+    }
+
+    @Test
+    void testFileOrder(){
 
     }
 }
